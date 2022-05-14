0759463fa26dd89e47382d78bc0441c9021be1db
Michael Webster <miketwebster@gmail.com>
2022-05-14 14:14:27 -0400

    Small change in cs_sound for updated Cvc on cinnamon-desktop.

--- files/usr/share/cinnamon/cinnamon-settings/modules/cs_sound.py
+++ files/usr/share/cinnamon/cinnamon-settings/modules/cs_sound.py
@@ -343,11 +343,9 @@ class VolumeLevelBar(SettingsWidget):
 
     def setStream(self, stream):
         if self.stream != None:
-            self.stream.remove_monitor()
             self.stream.disconnect(self.monitorId)
         self.stream = stream
-        self.stream.create_monitor()
-        self.monitorId = self.stream.connect("monitor-update", self.update)
+        self.monitorId = self.stream.connect("notify::state", self.update)
 
     def update(self, stream, value):
         if self.lastPeak >= DECAY_STEP and value < self.lastPeak - DECAY_STEP:
